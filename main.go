aca1e1ca4b9b5a913f99141244072708464a45fc
Julian Villegas <julian.villegasplus@gmail.com>
2020-06-01 17:06:35 -0500

    [ADD]main.go: Adding entry point of the program and check the connection to DB

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/julianchok25/dj_events_social_interaction/db"
+	"github.com/julianchok25/dj_events_social_interaction/handlers"
+)
+
+func main() {
+	if db.CheckConnection() == 0 {
+		log.Fatal("No connection to data base")
+		return
+	}
+	handlers.Handlers()
 }
