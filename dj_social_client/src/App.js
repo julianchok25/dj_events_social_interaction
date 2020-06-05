ae531419bc7ed048538d2162b5698d8a54ad5ecc
unknown <camiaraquecaro17@gmail.com>
2020-06-05 00:09:43 -0500

    [ADD]App Return User Exists or Not Exists

--- dj_social_client/src/App.js
+++ dj_social_client/src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import SigninSingUp from "./page/SigninSingUp";
 
-function App() {
+export default function App() {
+  const [user, Setuser] = useState({ name: "Camilo Araque" });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {user ? (
+        <div>
+          {" "}
+          <SigninSingUp />{" "}
+        </div>
+      ) : (
+        <h1>Not Logged</h1>
+      )}
     </div>
   );
 }
-
-export default App;
