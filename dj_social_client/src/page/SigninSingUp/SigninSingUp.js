e260428e091f6a7fe335703f7d76f6c1a19656ff
unknown <camiaraquecaro17@gmail.com>
2020-06-08 14:38:05 -0500

    [UPD]Adding New Component Right and SCSS

--- dj_social_client/src/page/SigninSingUp/SigninSingUp.js
+++ dj_social_client/src/page/SigninSingUp/SigninSingUp.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMusic,
+  faCalendar,
+  faUserCheck,
+  faCompactDisc,
+} from "@fortawesome/free-solid-svg-icons";
 import LogoVinyl from "../../assets/png/VinylRed.png";
+import LogoWhite from "../../assets/png/VinylWhite.png";
 import "./SigninSingUp.scss";
 
 export default function SigninSingUp() {
@@ -19,10 +27,22 @@ function LeftComponent() {
     <Col className="signin-signup__left" xs={6}>
       <img src={LogoVinyl} alt="Vinyl" />
       <div>
-        <h2>- Listen to what interests you.</h2>
-        <h2>- Schedule your Events.</h2>
-        <h2>- Hire your favorite djs.</h2>
-        <h2>- Make your music known.</h2>
+        <h2>
+          <FontAwesomeIcon icon={faMusic} />
+          Listen to what interests you.
+        </h2>
+        <h2>
+          <FontAwesomeIcon icon={faCalendar} />
+          Schedule your Events.
+        </h2>
+        <h2>
+          <FontAwesomeIcon icon={faUserCheck} />
+          Hire your favorite djs.
+        </h2>
+        <h2>
+          <FontAwesomeIcon icon={faCompactDisc} />
+          Make your music known.
+        </h2>
       </div>
     </Col>
   );
@@ -31,7 +51,13 @@ function LeftComponent() {
 function RightComponent() {
   return (
     <Col className="signin-signup__right" xs={6}>
-      <h1>RightComponent...</h1>
+      <div>
+        <img src={LogoWhite} alt="LogoWhite" />
+        <h2>See what your favorite djs are uploading right now</h2>
+        <h3>Join To Vinyl today.</h3>
+        <Button variant="danger">Register</Button>
+        <Button variant="outline-primary">Login</Button>
+      </div>
     </Col>
   );
 }
