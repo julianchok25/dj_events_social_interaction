067795d467d71d1547cca0ea03f7d9a6e58391f8
unknown <camiaraquecaro17@gmail.com>
2020-06-09 20:38:48 -0500

    [UPD]Adding More Functions SignInApi SignUpApi SetToken getTokenApi logoutApi isUserLoggedApi isExpiredToken

--- dj_social_client/src/api/auth.js
+++ dj_social_client/src/api/auth.js
@@ -1,4 +1,6 @@
-import { API_HOST } from "../utils/constants";
+import { API_HOST, TOKEN } from "../utils/constants";
+import jwtDecode from "jwt-decode";
+
 export function signUpApi(user) {
   const url = `${API_HOST}/sign-ins`;
   const userTemp = {
@@ -29,4 +31,72 @@ export function signUpApi(user) {
     .catch((err) => {
       return err;
     });
+}
+
+export function signInApi(user) {
+  const url = `${API_HOST}/logins`;
+
+  const data = {
+    ...user,
+    email: user.email.toLowerCase(),
+  };
+
+  const params = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  };
+
+  return fetch(url, params)
+    .then((response) => {
+      if (response.status >= 200 && response.status < 300) {
+        return response.json();
+      }
+      return { message: "Email or Password Incorrect" };
+    })
+    .then((result) => {
+      return result;
+    })
+    .catch((err) => {
+      return err;
+    });
+}
+
+export function setTokenApi(token) {
+  localStorage.setItem(TOKEN, token);
+}
+
+export function getTokenApi() {
+  return localStorage.getItem(TOKEN);
+}
+
+export function logoutApi() {
+  localStorage.removeItem(TOKEN);
+}
+
+export function isUserLoggedApi() {
+  const token = getTokenApi();
+
+  if (!token) {
+    logoutApi();
+    return null;
+  }
+  if (isExpiredToken(token)) {
+    logoutApi();
+  }
+  return jwtDecode(token);
+}
+
+function isExpiredToken(token) {
+  const { exp } = jwtDecode(token);
+  const expire = exp * 1000;
+  const timeout = expire - Date.now();
+
+  if (timeout < 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
