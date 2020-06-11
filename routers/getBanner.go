ef63e593c77d2d17f02c0a8a1ca3236b315097c4
Julian Villegas <julian.villegasplus@gmail.com>
2020-06-11 18:36:33 -0500

    [ADD]getAvatar&Banner: Adding end point for get the avatar and banner profile

---
+++ routers/getBanner.go
@@ -0,0 +1,35 @@
+package routers
+
+import (
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/julianchok25/dj_events_social_interaction/db"
+)
+
+/*GetBanner is the route to get the banner from the user ID and copy to the response writer*/
+func GetBanner(w http.ResponseWriter, r *http.Request) {
+	ID := r.URL.Query().Get("id")
+	if len(ID) < 1 {
+		http.Error(w, "You should to send the Id Parametter", http.StatusBadRequest)
+		return
+	}
+	// get the profile user
+	profile, err := db.SearchProfile(ID)
+	if err != nil {
+		http.Error(w, "User not found", http.StatusBadRequest)
+		return
+	}
+	// Reading the user avatar
+	openFile, err := os.Open("uploads/avatars/" + profile.Banner)
+	if err != nil {
+		http.Error(w, "Image not found", http.StatusBadRequest)
+		return
+	}
+
+	_, err = io.Copy(w, openFile)
+	if err != nil {
+		http.Error(w, "Error copying the image", http.StatusBadRequest)
+	}
+}
