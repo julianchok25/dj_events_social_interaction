a2479a75a88f2850b9d813460386e7f4e7753b0b
unknown <camiaraquecaro17@gmail.com>
2020-06-16 18:49:30 -0500

    [ADD]File Function Regular Expression URLS Posts

---
+++ dj_social_client/src/utils/functions.js
@@ -0,0 +1,4 @@
+export function replaceURLWithHTMLLinks(text) {
+  var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi;
+  return text?.replace(exp, "<a href='$1' target='_blank'>$1</a>");
+}
