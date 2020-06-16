a8bee77e7d9982785a1b193bfe125eb05557bc26
unknown <camiaraquecaro17@gmail.com>
2020-06-16 18:42:19 -0500

    [UPD]

--- dj_social_client/src/page/Home/Home.js
+++ dj_social_client/src/page/Home/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
 import BasicLayout from "../../layout/BasicLayout";
 
-export default function Home() {
+export default function Home(props) {
+  const { setRefreshCheckLogin } = props;
+
   return (
-    <BasicLayout className="home">
+    <BasicLayout className="home" setRefreshCheckLogin={setRefreshCheckLogin}>
       <h2>This is Home</h2>
     </BasicLayout>
   );
